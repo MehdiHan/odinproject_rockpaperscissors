aa4f46aec93e5688e0f6a5295603e83bd0da86a8
MehdiHan <meh.bouchard@outlook.fr>
2020-10-04 23:49:54 +0200

    Adding playRound display

--- js/app.js
+++ js/app.js
@@ -1,54 +1,46 @@
-const game = (rounds) => {
-  // game possibility choices
-  const choices = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('[data-value]');
+const resultsContainer = document.getElementById('resultsContainer');
+
+const game = () => {
   let playerScore = 0;
   let computerScore = 0;
-  let result = '';
-  let message = '';
-  // iteration for each round
-  for (let i = 0; i < rounds; i++) {
-    // random choice for the player
-    let playerSelection = prompt(
-      'Rock,Paper or Scissors ? Make your choice'
-    ).toLowerCase();
+  let choices = ['rock', 'paper', 'scissors'];
+  const div = document.createElement('div');
 
-    // random choice for the computer
-    const computerChoice = () => {
-      const index = Math.floor(Math.random() * Math.floor(choices.length));
-      return choices[index];
-    };
-    let computerSelection = computerChoice();
+  const computerChoice = () => {
+    const index = Math.floor(Math.random() * Math.floor(choices.length));
+    return choices[index];
+  };
 
-    const playRound = (playerSelection, computerSelection) => {
-      if (playerSelection === computerSelection) {
-        message = `${playerSelection} against ${computerSelection} ! We have a Tie Game`;
-      } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-      ) {
-        playerScore += 1;
-        message = `You Win ! ${playerSelection} beats ${computerSelection}`;
-      } else {
-        computerScore += 1;
-        message = `You Lose ! ${computerSelection} beats ${playerSelection}`;
-      }
-      return `You : ${playerScore} - CPU : ${computerScore}
+  const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+      message = `${playerSelection} against ${computerSelection} ! We have a Tie Game`;
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      playerScore += 1;
+      message = `You Win ! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      computerScore += 1;
+      message = `You Lose ! ${computerSelection} beats ${playerSelection}`;
+    }
+    return `You : ${playerScore} - CPU : ${computerScore}
    ${message}`;
-    };
-    console.log(playRound(playerSelection, computerSelection));
-  }
+  };
 
-  if (playerScore > computerScore) {
-    result = `You Win
-    You : ${playerScore} - CPU : ${computerScore}`;
-  } else if (playerScore < computerScore) {
-    result = `You Lose
-    You : ${playerScore} - CPU : ${computerScore}`;
-  } else {
-    result = `Both GG !! Tie Game`;
-  }
-  return result;
+  buttons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      div.innerHTML = '';
+      let playerSelection = event.target.dataset.value;
+      let computerSelection = computerChoice();
+
+      div.innerHTML = playRound(playerSelection, computerSelection);
+
+      resultsContainer.appendChild(div);
+    });
+  });
 };
 
-console.log(game(5));
+game();
