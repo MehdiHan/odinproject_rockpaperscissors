936e6813a77761a09ba0bdb240abb65ceaf55db9
MehdiHan <meh.bouchard@outlook.fr>
2020-10-05 08:43:47 +0200

    Adding some comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 const buttons = document.querySelectorAll('[data-value]');
 const resultsContainer = document.getElementById('resultsContainer');
-const div = document.createElement('div');
-const p = document.createElement('p');
 const playerScoreboard = document.getElementById('playerScoreboard');
 const computerScoreboard = document.getElementById('computerScoreboard');
+const div = document.createElement('div');
+const p = document.createElement('p');
 
+/**
+ * Playing a game according to the number of rounds
+ * rounds is set to 5 by default
+ * @param {number} rounds
+ */
 const game = (rounds) => {
   let playerScore = 0;
   let computerScore = 0;
@@ -12,11 +17,19 @@ const game = (rounds) => {
   let result = '';
   let choices = ['rock', 'paper', 'scissors'];
 
+  /**
+   * Generate random choice for computer
+   */
   const computerChoice = () => {
     const index = Math.floor(Math.random() * Math.floor(choices.length));
     return choices[index];
   };
 
+  /**
+   * Play a single Rock,Paper,Scissors Round
+   * @param {string} playerSelection
+   * @param {string} computerSelection
+   */
   const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
       message = `${playerSelection} against ${computerSelection} ! We have a Tie Game`;
@@ -44,6 +57,11 @@ const game = (rounds) => {
     resultsContainer.appendChild(div);
   };
 
+  /**
+   * Receive number of rounds
+   * Give the end game result
+   * @param {number} rounds
+   */
   const endGame = (rounds) => {
     if (round === rounds) {
       p.innerHTML = '';
@@ -59,11 +77,13 @@ const game = (rounds) => {
     playerScoreboard.innerText = playerScore;
     computerScoreboard.innerText = computerScore;
 
-    console.log(playerScore, computerScore, result, round);
-
     displayResult(result);
   };
 
+  /**
+   * Add click event on every buttons
+   * When button is clicked , round will start
+   */
   buttons.forEach((button) => {
     button.addEventListener('click', function (event) {
       if (round < rounds) {
@@ -79,9 +99,12 @@ const game = (rounds) => {
 
       displayRound(playRound(playerSelection, computerSelection));
       endGame(rounds);
-      console.log(round, rounds);
     });
   });
 };
 
+/**
+ * Call of function game
+ * You can change number of rounds here
+ */
 game(5);
