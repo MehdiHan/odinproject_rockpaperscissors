cefd0a491a2954b856b4853bafbbb34df73a012e
MehdiHan <meh.bouchard@outlook.fr>
2020-10-05 08:18:52 +0200

    Adding functions to display results in HTML, reading README.md

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # The Odin Project - Rock, Paper, Scissors
 
-- [x] HTML Structure
-- [x] CSS
-- [ ] JS : Add DOM events
-- [ ] README
+## Introduction
 
-Game is working through alert and console.log
+In this project we create a mini "Rock,Paper,Scissors" game.
+
+At the start of the project the game was played through the console.
+
+## Evolution
+
+I have add HTML structure and some CSS, now we can chain games.
+
+Each game will return final result from 5 rounds.
+
+## Sources
+
+- [Rock,Paper,Scissors : Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors)
--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 const buttons = document.querySelectorAll('[data-value]');
 const resultsContainer = document.getElementById('resultsContainer');
+const div = document.createElement('div');
+const p = document.createElement('p');
+const playerScoreboard = document.getElementById('playerScoreboard');
+const computerScoreboard = document.getElementById('computerScoreboard');
 
-const game = () => {
+const game = (rounds) => {
   let playerScore = 0;
   let computerScore = 0;
+  let round = 0;
+  let result = '';
   let choices = ['rock', 'paper', 'scissors'];
-  const div = document.createElement('div');
 
   const computerChoice = () => {
     const index = Math.floor(Math.random() * Math.floor(choices.length));
@@ -26,21 +31,57 @@ const game = () => {
       computerScore += 1;
       message = `You Lose ! ${computerSelection} beats ${playerSelection}`;
     }
-    return `You : ${playerScore} - CPU : ${computerScore}
-   ${message}`;
+    return `${message}`;
+  };
+
+  const displayResult = (result) => {
+    p.innerHTML = result;
+    resultsContainer.appendChild(p);
+  };
+
+  displayRound = (round) => {
+    div.innerHTML = round;
+    resultsContainer.appendChild(div);
+  };
+
+  const endGame = (rounds) => {
+    if (round === rounds) {
+      p.innerHTML = '';
+      if (playerScore > computerScore) {
+        result = `You Win this Game`;
+      } else if (playerScore < computerScore) {
+        result = `You Lose this Game`;
+      } else {
+        result = `Both GG !! Tie Game`;
+      }
+    }
+
+    playerScoreboard.innerText = playerScore;
+    computerScoreboard.innerText = computerScore;
+
+    console.log(playerScore, computerScore, result, round);
+
+    displayResult(result);
   };
 
   buttons.forEach((button) => {
     button.addEventListener('click', function (event) {
-      div.innerHTML = '';
+      if (round < rounds) {
+        round++;
+      } else {
+        round = 0;
+        playerScore = 0;
+        computerScore = 0;
+        result = '';
+      }
       let playerSelection = event.target.dataset.value;
       let computerSelection = computerChoice();
 
-      div.innerHTML = playRound(playerSelection, computerSelection);
-
-      resultsContainer.appendChild(div);
+      displayRound(playRound(playerSelection, computerSelection));
+      endGame(rounds);
+      console.log(round, rounds);
     });
   });
 };
 
-game();
+game(5);
